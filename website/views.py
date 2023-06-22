87b3131dc40dd195b6b4d7391961b8b85c4d71a4
elyaass74 <tesla.elyas@gmail.com>
2023-06-22 12:26:33 +0330

    first templates

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request , 'website/index.html')
+
 def about_view(request):
-    return HttpResponse('<h1>About me</h1>')
+    return render(request , 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact me</h1>')
-# Create your views here.
+    return render(request , 'website/contact.html')
