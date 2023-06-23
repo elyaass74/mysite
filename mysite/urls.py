014c7a24f3bffb5f53f1e0af0ac54767d2583bbd
elyaass74 <tesla.elyas@gmail.com>
2023-06-23 13:17:07 +0330

    added static dir ,root , url

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -26,3 +28,6 @@ urlpatterns = [
    
 
 ]
+#static('static','base'/static)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
