921477d0036a2d58b03c0294d15b6b4fc6175c8a
elyaass74 <tesla.elyas@gmail.com>
2023-06-25 18:49:15 +0330

    context

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def blog_detail(request):
-    return render(request, 'blog/blog-detail.html' ) 
+    context = {'title':'sajad new site is started' , 'content': 'thss site is foundated by elyas with django' , 'author':'elays'}
+    return render(request, 'blog/blog-detail.html',context ) 
 
 def blog_single(request):
     return render(request , 'blog/blog-single.html' )
